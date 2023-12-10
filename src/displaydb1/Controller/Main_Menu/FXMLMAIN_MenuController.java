3ccc6196c6c009f099826880c12c225f100dbd2d
Blizzard03 <99153189+Blizzard03@users.noreply.github.com>
2023-12-10 22:06:33 +0700

    update main menu controlller

--- src/displaydb1/Controller/Main_Menu/FXMLMAIN_MenuController.java
+++ src/displaydb1/Controller/Main_Menu/FXMLMAIN_MenuController.java
@@ -4,6 +4,10 @@
  */
 package displaydb1.Controller.Main_Menu;
 
+//Master Database Import
+import displaydb1.Data_Base.DBMahasiswa.DB_Mahasiswa;
+import displaydb1.Data_Base.DBMataKuliah.DB_MataKuliah;
+
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -25,6 +29,12 @@ import javafx.stage.WindowEvent;
  */
 public class FXMLMAIN_MenuController {
 
+    //Data Base Mahasiswa
+    public static DB_Mahasiswa dataBaseMahasiswa = new DB_Mahasiswa();
+    
+    //Data Base Mata Kuliah
+    public static DB_MataKuliah dataBaseMatakuliah = new DB_MataKuliah();
+    
     @FXML
     private MenuItem Data_Mahasiswa;
     @FXML
@@ -36,6 +46,31 @@ public class FXMLMAIN_MenuController {
 
     @FXML
     private void Mahasiswa_Table_View(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/displaydb1/FXML/List_Siswa/FXMLListData_Siswa.fxml"));
+            Parent root = (Parent) loader.load();
+            Scene scene = new Scene(root);
+            Stage stg = new Stage();
+            stg.setTitle("Tabel Data Mahasiswa");
+            stg.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                public void handle(final WindowEvent windowEvent) {
+                    Alert al = new Alert(Alert.AlertType.CONFIRMATION, "Are You Sure Want Quit The Program?", ButtonType.OK, ButtonType.CANCEL);
+                    al.showAndWait();
+                    if (al.getResult() == ButtonType.CANCEL) {
+                        windowEvent.consume();
+                    }
+                }
+            }
+            );
+            stg.initModality(Modality.APPLICATION_MODAL);
+            stg.setResizable(false);
+            stg.setIconified(false);
+            stg.setScene(scene);
+            stg.show();
+            Main_Menu.getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
